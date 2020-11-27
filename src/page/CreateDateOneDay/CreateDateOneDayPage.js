7104cd872c374b1d16c3c652cd3ab736074ffec2
SaharatPr <saharat.darm@gmail.com>
2020-11-27 21:05:35 +0700

    Update page my trip

--- src/page/CreateDateOneDay/CreateDateOneDayPage.js
+++ src/page/CreateDateOneDay/CreateDateOneDayPage.js
@@ -1,22 +1,45 @@
 import React from 'react'
 import {
-    View
+    View,
+    Text,
+    TextInput,
+    Image
 } from 'react-native';
 import{
-    Text
-} from '@ui-kitten/components'
+    ButtonGroup
+} from '@ui-kitten/components';
+import {styles} from './CreateDateOneDayeStyle'
 class CreateDateOneDayPage extends React.Component{
     constructor(props){
         super(props);
     }
 
     render(){
+        var x = "1/08/2020";
         return(
-            <View>
-                <Text>
-                    Create
+            <>
+            <View style={styles.container}>
+                <Text style={styles.txt} >
+                    Create your trip name
                 </Text>
+                <TextInput style={styles.txtinput}></TextInput>
+                <Text style={styles.txtdate} >
+                    Create your trip day
+                </Text>
+                <View style={styles.date}>
+
+                    <Image source={
+                        {uri:'https://drive.google.com/uc?export=view&id=16nAUJfo2Ae3hPntVhVNjbO1_eKLUEu_P'}
+                        } style={styles.imgcalenda}/>
+
+                    <Text style={styles.txttxtcalenda} >
+                        {x}
+                    </Text>
+
+
+                </View>
             </View>
+            </>
         );
     }
 }
---
+++ src/page/CreateDateOneDay/CreateDateOneDayeStyle.js
@@ -0,0 +1,61 @@
+import {StyleSheet} from 'react-native';
+const styles = StyleSheet.create(
+    {
+       container :{
+        flex: 1,
+        flexDirection: "column",
+        resizeMode: "cover",
+        justifyContent: "center",
+        alignItems: 'center',
+        },
+        txt:{
+            color : "#052238",
+            fontSize:30.0,
+            fontWeight:"bold"
+        },
+        txtinput: {
+            backgroundColor : '#E6E6E6',
+            width:"80%",
+            fontSize:25.0,
+            padding: "3%",
+            borderRadius:10,
+            textAlign: "center",
+            marginTop: "5%"
+        },
+        txtdate:{
+            color : "#052238",
+            fontSize:30.0,
+            fontWeight:"bold",
+            marginTop: "10%" 
+        }, 
+        date:{
+            backgroundColor : "#052238",
+            width : "50%",
+            resizeMode: "cover",
+            justifyContent: "center",
+            alignItems: 'center',
+            borderRadius: 10,
+            marginTop : "5%",
+            flexDirection: "row",
+            padding: "3%",
+        },
+        txttxtcalenda:{
+            color : "#FFFFFF",
+            fontSize: 18,
+            flexDirection: "column",
+        },
+        imgcalenda: {
+            width:20,
+            height: 20,
+            flexDirection: "row",
+            resizeMode: "cover",
+            justifyContent: "center",
+            alignItems: 'center',
+            flexDirection: "column",
+            marginRight:"10%"
+        }
+
+    }
+);
+
+export {styles};
